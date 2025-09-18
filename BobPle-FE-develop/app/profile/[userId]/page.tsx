313739a56d4c6f887595c28827e21a269bf46814
dlwjddhks <67868522+dlwjddhks@users.noreply.github.com>
2025-09-18 21:29:48 +0900

    Update page.tsx

--- BobPle-FE-develop/app/profile/[userId]/page.tsx
+++ BobPle-FE-develop/app/profile/[userId]/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 export const runtime = 'edge';
+export const dynamic = "force-dynamic";
 import { useEffect, useMemo, useState } from "react";
 import { useParams, useRouter } from "next/navigation";
 import { Button } from "@/components/ui/button";
