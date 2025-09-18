9394b42dc0792526f086f80c0a958d207f4310e4
dlwjddhks <67868522+dlwjddhks@users.noreply.github.com>
2025-09-18 20:30:56 +0900

    Update page.tsx

--- BobPle-FE-develop/app/restaurants/[restaurantId]/page.tsx
+++ BobPle-FE-develop/app/restaurants/[restaurantId]/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+export const runtime = 'edge';
 import { useEffect, useMemo, useState } from "react";
 import { useParams, useRouter, useSearchParams } from "next/navigation";
 import { Button } from "@/components/ui/button";
