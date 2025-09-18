a82039a406f287013ea0bf94675e694ec7075953
dlwjddhks <67868522+dlwjddhks@users.noreply.github.com>
2025-09-18 20:27:43 +0900

    Update next.config.mjs

--- BobPle-FE-develop/next.config.mjs
+++ BobPle-FE-develop/next.config.mjs
@@ -1,14 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  images: {
-    unoptimized: true,
-  },
+  eslint: { ignoreDuringBuilds: true },
+  typescript: { ignoreBuildErrors: true },
+  images: { unoptimized: true },
+
+  // ✅ 추가
+  output: 'export',
 }
 
 export default nextConfig
